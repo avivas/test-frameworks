8162325e741e9d751f84eefb4039a6cfa8667ca9
Alejandro Vivas <alejo.vivas@gmail.com>
2021-03-12 08:39:39 -0500

    Fix http method

--- performance/crud-mysql/crud-mysql-java-vertx/wrk2-put.lua
+++ performance/crud-mysql/crud-mysql-java-vertx/wrk2-put.lua
@@ -2,7 +2,7 @@
 wrk.scheme  = "http"
 wrk.host    = "127.0.0.1"
 wrk.port    = 8081
-wrk.method  = "POST"
+wrk.method  = "PUT"
 wrk.path    = "/"
 wrk.headers = {}
 wrk.body    = '{"description":"description-put","name":"name-put"}'
