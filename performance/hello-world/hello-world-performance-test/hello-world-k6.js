25c94f3baa03c3a802aa6707319f952a25bbb857
Alejandro Vivas <alejo.vivas@gmail.com>
2021-03-30 20:02:54 -0500

    Improve k6 hello world test

--- performance/hello-world/hello-world-performance-test/hello-world-k6.js
+++ performance/hello-world/hello-world-performance-test/hello-world-k6.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export let options = {
-  vus: 26000,
-  duration: '90s',
+  vus: 100,
+  duration: '60s',
 };
 
 export default function () {
