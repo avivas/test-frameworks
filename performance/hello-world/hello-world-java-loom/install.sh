edf76303444f6558edad37909f37fc773cf20312
Alejandro Vivas <alejo.vivas@gmail.com>
2021-06-18 17:38:46 -0500

    add quient mode on install scripts

--- performance/hello-world/hello-world-java-loom/install.sh
+++ performance/hello-world/hello-world-java-loom/install.sh
@@ -2,5 +2,5 @@
 
 source $HOME/.sdkman/bin/sdkman-init.sh
 
-sdk install java 17.ea.7.lm-open
-sdk install maven 3.6.3
+sdk install java 17.ea.7.lm-open < /dev/null
+sdk install maven 3.6.3 < /dev/null
--- performance/hello-world/hello-world-java-vertx/install.sh
+++ performance/hello-world/hello-world-java-vertx/install.sh
@@ -2,5 +2,5 @@
 
 source $HOME/.sdkman/bin/sdkman-init.sh
 
-sdk install java 15.0.1.hs-adpt
-sdk install maven 3.6.3
+sdk install java 15.0.1.hs-adpt < /dev/null
+sdk install maven 3.6.3 < /dev/null
