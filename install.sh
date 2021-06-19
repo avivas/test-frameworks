bcdf0a6f8d557d5a3d6c5d54a6cb7d0eb5c67c1c
Alejandro Vivas <alejo.vivas@gmail.com>
2021-06-18 20:05:23 -0500

    update k6 install function

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ function install_k6 {
   if ! command -v k6 &> /dev/null 
   then
     echo "Start: Install k6 ----------------------------------------------" 
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
-    echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
+
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+    echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
     sudo apt-get update
     sudo apt-get install k6 -y
+
     echo "End  : Install k6 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" 
   else
     echo "Using installed k6"
