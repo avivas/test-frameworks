34b645a72bf2a88303d52653b26edfc3ecde589c
Alejandro Vivas <alejo.vivas@gmail.com>
2021-07-05 18:59:51 -0500

    fix java 17 version

--- hello-world/hello-world-java-loom/build.sh
+++ hello-world/hello-world-java-loom/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 . ~/.sdkman/bin/sdkman-init.sh
-sdk use java 17.ea.2.lm-open < /dev/null
+sdk use java 17.ea.7.lm-open < /dev/null
 sdk use maven 3.6.3 < /dev/null
 
 echo "Build hello-world-java-loom"
