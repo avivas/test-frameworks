c6b02c25980d171beda39787379a1bb941111663
Alejandro Vivas <alejo.vivas@gmail.com>
2021-07-05 19:10:00 -0500

    fix java version

--- crud-mysql/crud-mysql-java-loom/build.sh
+++ crud-mysql/crud-mysql-java-loom/build.sh
@@ -2,7 +2,7 @@
 
 echo "Using java 17 loom"
 . ~/.sdkman/bin/sdkman-init.sh
-sdk use java 17.ea.2.lm-open < /dev/null
+sdk use java java 17.ea.7.lm-open < /dev/null
 sdk use maven 3.6.3 < /dev/null
 
 
