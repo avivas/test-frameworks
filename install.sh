1f912e4c75d96498ab11a1fe9bd42497c2c2b0d3
Alejandro Vivas <alejo.vivas@gmail.com>
2021-07-05 19:52:52 -0500

    Add install docker

--- install.sh
+++ install.sh
@@ -65,8 +65,30 @@ function install_rustup {
 }
 #-------------------------
 
+#-> Install docker
+function install_docker {
+
+  sudo apt-get update
+
+  sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+  echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io
+}
 
 #-> Install tools
+install_docker
 install_k6
 install_sdkman
 install_gvm
